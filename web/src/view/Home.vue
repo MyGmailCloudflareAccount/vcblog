45fb0308cbbb070eab5fe7a2f6512949d7b7f961
Your Name <you@example.com>
2026-05-08 10:44:43 +0800

    ``` chore(config): 移除编辑器配置文件并更新许可证格式
    移除了 .editorconfig 和 .prettierrc 配置文件，
    同时调整了 LICENSE 文件的格式以符合标准 MIT 许可证模板。
    ```

--- web/src/view/Home.vue
+++ web/src/view/Home.vue
@@ -5,19 +5,34 @@ const siteStore = useSiteStore()
 import { useRouter } from 'vue-router'
 const router = useRouter()
 
-import { watch, onMounted } from 'vue'
+import { watch, ref, onMounted } from 'vue'
+const maxPage = ref(1)
+const curPage = ref(1)
+const loading = ref(false)
+const posts = ref([])
+
 onMounted(async () => {
     const resp = await fetch('/api/post/count')
 
     const postCount = (await resp.json()).count
     const postPerPage = siteStore.site.post_per_page
-    const maxPage = Math.ceil(postCount / postPerPage)
+    maxPage.value = Math.ceil(postCount / postPerPage)
 
     watch(
         () => router.currentRoute.value.query.page,
         async (page, _) => {
             if (!page) page = 1
+            if (typeof page !== 'number') page = parseInt(page, 10)
+            if (isNaN(page) || page < 1) page = 1
+            if (page > maxPage.value) page = maxPage.value
+
             document.title = `${page === 1 ? '首页' : `第 ${page} 页`} | ${siteStore.site.title}`
+            curPage.value = page
+
+            loading.value = true
+            const resp = await fetch(`/api/post/list?page=${page}`)
+            posts.value = await resp.json()
+            loading.value = false
         },
         { immediate: true }
     )
@@ -25,6 +40,7 @@ onMounted(async () => {
 </script>
 
 <template>
+    <h2 style="margin-top: 5px" v-if="loading">加载中...</h2>
     <div v-for="i in 100">
         <router-link :to="`/?page=${i}`">Page {{ i }}</router-link>
     </div>
