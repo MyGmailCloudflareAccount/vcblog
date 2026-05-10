608d7863fa9d416f8b1f3ac948608afab2c1e7cc
Your Name <you@example.com>
2026-05-10 19:42:46 +0800

    feat: set dynamic document title in dashboard based on site store

--- web/src/view/Dash.vue
+++ web/src/view/Dash.vue
@@ -1,4 +1,8 @@
-<script setup></script>
+<script setup>
+import { useSiteStore } from '@/stores/site'
+const siteStore = useSiteStore()
+document.title = `管理面板 | ${siteStore.site.title}`
+</script>
 
 <template>dashboard</template>
 
