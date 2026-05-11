a9087ab9d3169ff717793de3977ba38f0d81f497
Your Name <you@example.com>
2026-05-11 21:00:01 +0800

    feat: implement settings management with loading state and update functionality

--- web/src/view/dashboard/Settings.vue
+++ web/src/view/dashboard/Settings.vue
@@ -1,5 +1,30 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
 
-<template>settings tab</template>
+const loading = ref(false)
+const settings = ref({})
+
+onMounted(async () => {
+    loading.value = true
+    const resp = await fetch('/api/site/settings/list')
+    settings.value = await resp.json()
+    loading.value = false
+})
+
+const updateSettings = async () => {
+    console.log(settings.value)
+}
+</script>
+
+<template>
+    <el-form v-loading="loading" label-width="auto">
+        <el-form-item v-for="(_, key) in settings" :label="key">
+            <el-input v-model="settings[key]" placeholder=" " clearable />
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="updateSettings">更新</el-button>
+        </el-form-item>
+    </el-form>
+</template>
 
 <style scoped></style>
