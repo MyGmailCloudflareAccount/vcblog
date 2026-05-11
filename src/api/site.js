128520ccd9b54b1e81ac9b641217600fb039bbd5
Your Name <you@example.com>
2026-05-11 20:30:30 +0800

    refactor: reorganize config routes under settings and implement list endpoint

--- src/api/site.js
+++ src/api/site.js
@@ -14,37 +14,16 @@ site.get('/info', async (req, res) => {
     })
 })
 
-const cfg = express.Router()
-site.use('/config', cfg)
+const settings = express.Router()
+site.use('/settings', settings)
 
 import { requireAuth } from './auth.js'
-cfg.use(requireAuth)
+settings.use(requireAuth)
 
-cfg.get('/list', async (req, res) => {
-    const keys = (await config.list()).keys
-    res.json(keys)
-})
-
-cfg.get('/value', async (req, res) => {
-    const key = req.query.key
-    if (typeof key !== 'string' || key === '') {
-        res.sendStatus(400)
-        return
-    }
-
-    const value = await config.get(key)
-    res.send(value)
-})
-
-cfg.post('/update', async (req, res) => {
-    const { key, value } = req.body
-    if (typeof key !== 'string' || key === '' || typeof value !== 'string' || value === '') {
-        res.sendStatus(400)
-        return
-    }
-
-    await config.put(key, value)
-    res.sendStatus(200)
+settings.get('/list', async (req, res) => {
+    const keys = (await config.list()).keys.map(item => item.name)
+    const values = await config.get(keys)
+    res.json(Object.fromEntries(values))
 })
 
 export default site
