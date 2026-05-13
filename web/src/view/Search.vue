c2a23f7105ee1ce7dc98ed50d056e96362b05a7f
Your Name <you@example.com>
2026-05-13 20:29:25 +0800

    feat: enhance search component with Element Plus UI elements for better user experience

--- web/src/view/Search.vue
+++ web/src/view/Search.vue
@@ -17,18 +17,15 @@ const search = async () => {
 </script>
 
 <template>
-    <div style="display: flex; align-items: center; margin-bottom: 5px; column-gap: 5px">
-        <button style="cursor: pointer" type="button" @click="search">搜索</button>
-        <input style="flex: 1" type="text" placeholder="输入关键词" v-model="input" />
+    <div style="display: flex; align-items: center; margin: 5px 0; column-gap: 5px">
+        <el-button plain :loading="loading" @click="search">搜索</el-button>
+        <el-input v-model="input" style="flex: 1" placeholder="输入关键词" />
     </div>
-    <h2 style="margin-top: 5px" v-if="loading">加载中...</h2>
-    <div v-else>
-        <div v-for="post in results">
-            <router-link :to="`/post/${post.id}`">
-                <h1 style="margin: 5px 0">{{ post.title }}</h1>
-            </router-link>
-            <p style="margin: 0; border-bottom: 1px solid black">{{ post.preview }}</p>
-        </div>
+    <div v-for="post in results">
+        <router-link :to="`/post/${post.id}`">
+            <h1 style="margin: 5px 0">{{ post.title }}</h1>
+        </router-link>
+        <p style="margin: 0; border-bottom: 1px solid black">{{ post.preview }}</p>
     </div>
 </template>
 
