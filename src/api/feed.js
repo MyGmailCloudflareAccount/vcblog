e113ed5f96fe6f8931abbdf8d130ba10874f4dc5
Your Name <you@example.com>
2026-05-14 16:57:53 +0800

    feat: add feed API with RSS and Atom endpoints

---
+++ src/api/feed.js
@@ -0,0 +1,55 @@
+import express from 'express'
+const feed = express.Router()
+
+import { get_config } from '../env/config.js'
+const config = get_config()
+
+import db from '../database/orm.js'
+import table from '../database/schema.js'
+import { desc, eq, sql } from 'drizzle-orm'
+
+import { Feed } from 'feed'
+
+const getFeed = async () => {
+    const feed = new Feed({
+        title: await config.get('title')
+    })
+
+    const posts = await db
+        .select({
+            id: table.id,
+            title: table.title,
+            preview: sql`substr(${table.content}, 1, 200)`.mapWith(String).as('preview'),
+            content: table.content
+        })
+        .from()
+        .where(eq(table.type, 'post'))
+        .orderBy(desc(table.id))
+        .limit(20)
+
+    posts.forEach(post => {
+        feed.addItem({
+            title: post.title,
+            id: post.id,
+            link: `/post/${post.id}`,
+            description: post.preview,
+            content: post.content
+        })
+    })
+
+    return feed
+}
+
+feed.get('/rss', async (req, res) => {
+    const feed = await getFeed()
+    res.type('application/xml')
+    res.send(feed.rss2())
+})
+
+feed.get('/atom', async (req, res) => {
+    const feed = await getFeed()
+    res.type('application/xml')
+    res.send(feed.atom1())
+})
+
+export default feed
